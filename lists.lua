dd104628da932cdfcb9aa247341a831c19c8c850
Birthday X Scripts <87570691+BirthScripts@users.noreply.github.com>
2022-09-21 19:09:00 +0800

    Update lists.lua

--- lists.lua
+++ lists.lua
@@ -1,5 +1,5 @@
 return {
 
-["birthday"] = {"6fccc89239f1aa0efeae538722d00d2a936ff12d", {"NERD FOUNDER", "FF5F5F"}},
+["birthday"] = {"246a756f78f90fd7131b365c630d536bab9b2b00f4dbb1051ac2ec4f53e34052fb781ff75929f7ab664c48ab8a174ce20cb845069732cb83ecef72cefc888a8f", {"NERD FOUNDER", "FF5F5F"}},
 
 }
